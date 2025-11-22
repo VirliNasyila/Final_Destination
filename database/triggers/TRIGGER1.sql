4ca81a5fbe6120539406f7283dc37ffc7efdf2a3
VirliNasyila <virlinasyila13@gmail.com>
2025-11-22 07:02:41 +0700

    Update TRIGGER1.sql
    Trigger follow–unfollow artis

--- database/triggers/TRIGGER1.sql
+++ database/triggers/TRIGGER1.sql
@@ -28,6 +28,7 @@ EXECUTE FUNCTION update_collection_rating();
 
 SELECT * FROM reviews;
 
+-- cek
 -- insert collection_rating
 INSERT INTO reviews (review, rating, review_id, user_id, collection_id)
 VALUES ('bagussss', 5, 1, 10, 1);
@@ -41,3 +42,51 @@ WHERE review_id = 1;
 
 -- delete review
 DELETE FROM reviews WHERE review_id = 1;
+
+
+
+-- Trigger follow–unfollow artis
+CREATE OR REPLACE FUNCTION update_artist_follow_count()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- FOLLOW → INSERT
+    IF TG_OP = 'INSERT' THEN
+        UPDATE artists
+        SET follower_count = COALESCE(follower_count, 0) + 1
+        WHERE artist_id = NEW.artist_id;
+    END IF;
+
+    -- UNFOLLOW → DELETE
+    IF TG_OP = 'DELETE' THEN
+        UPDATE artists
+        SET follower_count = COALESCE(follower_count, 0) - 1
+        WHERE artist_id = OLD.artist_id;
+    END IF;
+
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+-- trigger
+CREATE TRIGGER trg_update_artist_follow_count
+AFTER INSERT OR DELETE
+ON follow_artists   
+FOR EACH ROW
+EXECUTE FUNCTION update_artist_follow_count();
+
+-- cek
+-- follow
+INSERT INTO follow_artists (user_id, artist_id)
+VALUES (9, 4);
+
+INSERT INTO follow_artists (user_id, artist_id)
+VALUES (10, 4);
+
+-- unfollow
+DELETE FROM follow_artists
+WHERE user_id = 9 AND artist_id = 4;
+DELETE FROM follow_artists
+
+SELECT follower_count FROM artists WHERE artist_id = 4;
+
+SELECT * FROM artists;
