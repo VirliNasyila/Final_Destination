b90f4685bf6502821cff1d2f8d9506e2816fb8c1
Arney1 <work.henna656@passinbox.com>
2025-11-26 12:11:00 +0700

    Permit tour date equal to today in trigger

--- database/triggers/trg_validate_tour_date.sql
+++ database/triggers/trg_validate_tour_date.sql
@@ -2,8 +2,8 @@
 CREATE OR REPLACE FUNCTION validate_tour_date()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF NEW.tour_date <= CURRENT_DATE THEN
-        RAISE EXCEPTION 'Tour date (%s) must be after today (%s)', NEW.tour_date, CURRENT_DATE;
+    IF NEW.tour_date < CURRENT_DATE THEN
+        RAISE EXCEPTION 'Tour date (%s) shouldnt be before today (%s)', NEW.tour_date, CURRENT_DATE;
     END IF;
 
     RETURN NEW;
