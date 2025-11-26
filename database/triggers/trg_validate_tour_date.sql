b659d0d5c135b2c5cf383c09d2119dc128f9ce47
Arney1 <work.henna656@passinbox.com>
2025-11-26 11:54:08 +0700

    Add trigger to validate tour date
    tour_date > current_date

---
+++ database/triggers/trg_validate_tour_date.sql
@@ -0,0 +1,17 @@
+
+CREATE OR REPLACE FUNCTION validate_tour_date()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.tour_date <= CURRENT_DATE THEN
+        RAISE EXCEPTION 'Tour date (%s) must be after today (%s)', NEW.tour_date, CURRENT_DATE;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER trg_validate_tour_date
+BEFORE INSERT OR UPDATE ON tours
+FOR EACH ROW
+EXECUTE FUNCTION validate_tour_date();
