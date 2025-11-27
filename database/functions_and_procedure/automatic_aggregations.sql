5f8a871d3ff2d68275752d253f97fdfce00e722a
mailvlous <mailvelous.career@gmail.com>
2025-11-27 21:20:41 +0700

    update pokoknya

--- database/functions_and_procedure/automatic_aggregations.sql
+++ database/functions_and_procedure/automatic_aggregations.sql
@@ -12,34 +12,48 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
-CREATE OR REPLACE PROCEDURE update_song_rating(p_song_id INT)
-LANGUAGE plpgsql
-AS $$
+CREATE OR REPLACE FUNCTION get_playlist_duration_minutes(p_playlist_id INT)
+RETURNS INT AS $$
 DECLARE
-    avg_rating NUMERIC;
+    v_total_sec INT;
 BEGIN
-    SELECT get_song_average_rating(p_song_id)
-    INTO avg_rating;
+    SELECT SUM(s.song_duration)
+    INTO v_total_sec
+    FROM songs s
+    JOIN add_songs_playlists asp ON s.song_id = asp.song_id
+    WHERE asp.playlist_id = p_playlist_id;
 
-    UPDATE songs
-    SET song_rating = avg_rating
-    WHERE song_id = p_song_id;
+    RETURN COALESCE(v_total_sec / 60, 0);
 END;
-$$;
+$$ LANGUAGE plpgsql;
 
-
-CREATE OR REPLACE PROCEDURE update_collection_rating(p_collection_id INT)
-LANGUAGE plpgsql
-AS $$
+CREATE OR REPLACE FUNCTION get_artist_total_plays(p_artist_id INT)
+RETURNS BIGINT AS $$
 DECLARE
-    avg_rating NUMERIC;
+    v_total_plays BIGINT;
 BEGIN
-    SELECT get_collection_average_rating(p_collection_id)
-    INTO avg_rating;
+    SELECT COUNT(l.listen_id)
+    INTO v_total_plays
+    FROM create_songs cs
+    JOIN listens l ON cs.song_id = l.song_id
+    WHERE cs.artist_id = p_artist_id;
 
-    UPDATE collections
-    SET collection_rating = avg_rating
-    WHERE collection_id = p_collection_id;
+    RETURN COALESCE(v_total_plays, 0);
 END;
-$$;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION get_album_total_duration(p_collection_id INT)
+RETURNS INT AS $$
+DECLARE
+    v_total_seconds INT;
+BEGIN
+    -- Aggregation: SUM (Menjumlahkan nilai durasi)
+    SELECT SUM(s.song_duration)
+    INTO v_total_seconds
+    FROM collections_songs cs
+    JOIN songs s ON cs.song_id = s.song_id
+    WHERE cs.collection_id = p_collection_id;
+
+    RETURN COALESCE(v_total_seconds, 0);
+END;
+$$ LANGUAGE plpgsql;
