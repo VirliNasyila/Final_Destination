97645de95182d0a6ea36b3836faac15d145abcc8
nugrinti <nurahmaa.r@gmail.com>
2025-11-28 11:47:45 +0700

    Update trg_update_collection_top3_genres.sql

--- database/triggers/trg_update_collection_top3_genres.sql
+++ database/triggers/trg_update_collection_top3_genres.sql
@@ -35,22 +35,29 @@ FOR EACH ROW
 EXECUTE FUNCTION update_collection_top3_genres();
 
 
--- SELECT * FROM collections_songs;
+SELECT * FROM collections_songs;
 -- -- cek
 -- -- Update supaya trigger jalan
--- UPDATE collections_songs
--- SET nomor_track = nomor_track
--- WHERE collection_id = 1;
+UPDATE collections_songs
+SET nomor_track = nomor_track
+WHERE collection_id = 1;
 
--- UPDATE collections_songs
--- SET nomor_track = nomor_track
--- WHERE collection_id = 2;
+UPDATE collections_songs
+SET nomor_track = nomor_track
+WHERE collection_id = 2;
 
 -- -- Lihat hasil top 3 genre
--- SELECT * FROM collection_top_3_genres
--- WHERE collection_id = 1;
+SELECT * FROM collection_top_3_genres
+WHERE collection_id = 1;
 
--- SELECT * FROM collection_top_3_genres
--- WHERE collection_id = 2;
 
--- SELECT * FROM collection_top_3_genres;
+select * from collections_songs where collection_id = 1
+SELECT * FROM collection_top_3_genres
+WHERE collection_id = 2;
+
+SELECT * FROM collection_top_3_genres;
+INSERT INTO collections_songs (collection_id, song_id, nomor_disc, nomor_track) 
+VALUES(1, 16, 1, 99)
+SELECT * FROM genres
+SELECT * FROM songs_genres
+select * from genres
